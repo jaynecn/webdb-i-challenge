4293c1ca0497e4b562cc74ddf34fe883b6f0994d
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-11-04 14:55:32 +0000

    all CRUD endpoints created for accounts.  MVP achieved

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -7,8 +7,53 @@ const router = express.Router();
 router.get('/', (req, res) => {
   db('accounts')
     .then(result => {
-      console.log(result);
-      res.json({ message: 'CONTACT!'})
+      res.json({ message: 'You have made contact with the accounts api'})
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error message: ' + error.message });
+    })
+});
+
+
+router.get('/:id', (req, res) => {
+  // SELECT * FROM posts WHERE id = id;
+  db('accounts').where({ id: req.params.id })
+    .then(result => {
+      res.json(result[0])
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error message: ' + error.message });
+    })
+});
+
+router.post('/', (req, res) => {
+  // SELECT * FROM posts WHERE id = id;
+  db('accounts').insert({ name: req.body.name, budget: req.body.budget })
+    .then(result => {
+      res.json({ message:  'Account created with id number ' + result});
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error message: ' + error.message });
+    })
+});
+
+router.put('/:id', (req, res) => {
+  db('accounts').where({ id: req.params.id }).update({ name: req.body.name, budget: req.body.budget})
+    .then(result => {
+      res.json(`Account number ${req.params.id} updated`);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error message: ' + error.message });
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  db('accounts').where({ id: req.params.id}).del()
+    .then(result => {
+      res.json(`Account number ${req.params.id} deleted`);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error message: ' + error.message });
     })
 })
 
